c1ce5376d79540d8543d7ac546630f12a25d3d68
alexmaryin <java73@yandex.ru>
2021-02-03 15:15:12 +0400

    Pytest module and some database tests added

--- test/db_tests.py
+++ test/db_tests.py
@@ -1,5 +1,6 @@
 import pytest
 import sqlalchemy as sa
+from sqlalchemy.exc import IntegrityError
 from sqlalchemy.orm import scoped_session, sessionmaker
 
 from data.model.model import Base, Group
@@ -38,3 +39,13 @@ def test_new_group_should_be_added(db_session):
     group_from_base = db_session.query(Group).first()
     print(group_from_base.ID, group_from_base.group_name)
     assert group.group_name == group_from_base.group_name
+
+
+def test_fail_with_similar_group_id(db_session):
+    group_from_base = db_session.query(Group).first()
+    group = Group(ID=group_from_base.ID, group_name='Group2')
+    db_session.add(group)
+    with pytest.raises(IntegrityError):
+        db_session.commit()
+
+
