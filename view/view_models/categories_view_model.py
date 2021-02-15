1795735ef00b51b5cba6f7f9568822c64acf4de1
alexmaryin <java73@yandex.ru>
2021-02-15 16:41:14 +0400

    Some common methods unified in abstract view model

--- view/view_models/categories_view_model.py
+++ view/view_models/categories_view_model.py
@@ -1,74 +1,44 @@
-from kivy.uix.boxlayout import BoxLayout
 from kivymd.toast import toast
-from kivymd.uix.button import MDFlatButton
-from kivymd.uix.dialog import MDDialog
 from data.model.model import ObjectType
-from view.common_confirmation import ConfirmDialog
 from data.repository.result import Result
+from view.common_confirmation import ConfirmDialog
+from view.view_models.abstract_view_model import AbstractViewModel
 
 
-class CategoryEditor(BoxLayout):
-    def __init__(self, cancel_callback, ok_callback, **kwargs):
-        super().__init__(**kwargs)
-        self.cancel_callback = cancel_callback
-        self.ok_callback = ok_callback
-        self.category = None
-
-
-class CategoryViewModel:
+class CategoryViewModel(AbstractViewModel):
     def __init__(self, repository, refresh_view_callback):
-        self.repo = repository
-        self.refresh_view = refresh_view_callback
-        self.editor = None
-        self.category_dialog = None
-
-    def create_dialog(self):
-        return MDDialog(
-            title='Тип объекта интеллектуальной собственности:',
-            type='custom',
-            content_cls=self.editor,
-            buttons=[
-                MDFlatButton(text='Отмена', on_release=self.editor.cancel_callback),
-                MDFlatButton(text='Записать', on_release=self.editor.ok_callback)]
+        super().__init__(
+            repository,
+            refresh_view_callback,
+            dialog_title='Тип объекта интеллектуальной собственности:',
+            view='Типы объектов',
+            hint_dialog='название вида объектов интеллектуальной собственности'
         )
 
-    def close_dialog(self, instance):
-        self.category_dialog.content_cls.category_property = ''
-        self.category_dialog.dismiss()
-        self.category_dialog = None
-        self.editor = None
-
     def add(self, instance):
-        new = self.category_dialog.content_cls.category_property
+        new = self.editor_dialog.content_cls.name_property
         result, result_text = self.repo.add_category(new)
         if result == Result.SUCCESS:
-            self.category_dialog.content_cls.category_property = ''
-            self.category_dialog.dismiss()
+            self.editor_dialog.content_cls.name_property = ''
+            self.editor_dialog.dismiss()
             self.refresh_view("Типы объектов")
         toast(result_text)
 
     def on_add_enter(self):
-        self.editor = CategoryEditor(self.close_dialog, self.add)
-        self.category_dialog = self.create_dialog()
-        self.category_dialog.set_normal_height()
-        self.category_dialog.open()
+        super().add_dialog_enter(self.add)
 
     def edit(self, instance):
-        self.editor.category.name = self.category_dialog.content_cls.category_property
-        result, result_text = self.repo.edit_category(self.editor.category)
+        category = self.editor.item
+        category.name = self.editor_dialog.content_cls.name_property
+        result, result_text = self.repo.edit_category(category)
         if result == Result.SUCCESS:
-            self.category_dialog.content_cls.category_property = ''
-            self.category_dialog.dismiss()
+            self.editor_dialog.content_cls.name_property = ''
+            self.editor_dialog.dismiss()
             self.refresh_view("Типы объектов")
         toast(result_text)
 
     def on_edit_enter(self, category: ObjectType):
-        self.editor = CategoryEditor(self.close_dialog, self.edit)
-        self.editor.category = category
-        self.category_dialog = self.create_dialog()
-        self.category_dialog.content_cls.category_property = category.name
-        self.category_dialog.set_normal_height()
-        self.category_dialog.open()
+        super().edit_dialog_enter(category, category.name, self.edit)
 
     def on_delete_enter(self, category: ObjectType):
         ConfirmDialog(f'Удалить тип объекта {category.name}?', self.confirmed_delete, category)
--- view/view_models/group_view_model.py
+++ view/view_models/group_view_model.py
@@ -1,74 +1,44 @@
-from kivy.uix.boxlayout import BoxLayout
 from kivymd.toast import toast
-from kivymd.uix.button import MDFlatButton
-from kivymd.uix.dialog import MDDialog
 from data.model.model import Group
-from view.common_confirmation import ConfirmDialog
 from data.repository.result import Result
+from view.common_confirmation import ConfirmDialog
+from view.view_models.abstract_view_model import AbstractViewModel
 
 
-class GroupEditor(BoxLayout):
-    def __init__(self, cancel_callback, ok_callback, **kwargs):
-        super().__init__(**kwargs)
-        self.cancel_callback = cancel_callback
-        self.ok_callback = ok_callback
-        self.group = None
-
-
-class GroupViewModel:
+class GroupViewModel(AbstractViewModel):
     def __init__(self, repository, refresh_view_callback):
-        self.repo = repository
-        self.refresh_view = refresh_view_callback
-        self.editor = None
-        self.group_dialog = None
-
-    def create_dialog(self):
-        return MDDialog(
-            title='Группа:',
-            type='custom',
-            content_cls=self.editor,
-            buttons=[
-                MDFlatButton(text='Отмена', on_release=self.editor.cancel_callback),
-                MDFlatButton(text='Записать', on_release=self.editor.ok_callback)]
+        super().__init__(
+            repository,
+            refresh_view_callback,
+            dialog_title='Группа:',
+            view="Группы",
+            hint_dialog="Название группы"
         )
 
-    def close_dialog(self, instance):
-        self.group_dialog.content_cls.name_property = ''
-        self.group_dialog.dismiss()
-        self.group_dialog = None
-        self.editor = None
-
     def add(self, instance):
-        new = self.group_dialog.content_cls.name_property
+        new = self.editor_dialog.content_cls.name_property
         result, result_text = self.repo.add_group(new)
         if result == Result.SUCCESS:
-            self.group_dialog.content_cls.name_property = ''
-            self.group_dialog.dismiss()
+            self.editor_dialog.content_cls.name_property = ''
+            self.editor_dialog.dismiss()
             self.refresh_view("Группы")
         toast(result_text)
 
     def on_add_enter(self):
-        self.editor = GroupEditor(self.close_dialog, self.add)
-        self.group_dialog = self.create_dialog()
-        self.group_dialog.set_normal_height()
-        self.group_dialog.open()
+        super().add_dialog_enter(self.add)
 
     def edit(self, instance):
-        self.editor.group.group_name = self.group_dialog.content_cls.name_property
-        result, result_text = self.repo.edit_group(self.editor.group)
+        group = self.editor.item
+        group.group_name = self.editor_dialog.content_cls.name_property
+        result, result_text = self.repo.edit_group(group)
         if result == Result.SUCCESS:
-            self.group_dialog.content_cls.name_property = ''
-            self.group_dialog.dismiss()
+            self.editor_dialog.content_cls.name_property = ''
+            self.editor_dialog.dismiss()
             self.refresh_view("Группы")
         toast(result_text)
 
     def on_edit_enter(self, group: Group):
-        self.editor = GroupEditor(self.close_dialog, self.edit)
-        self.editor.group = group
-        self.group_dialog = self.create_dialog()
-        self.group_dialog.content_cls.name_property = group.group_name
-        self.group_dialog.set_normal_height()
-        self.group_dialog.open()
+        super().edit_dialog_enter(group, group.group_name, self.edit)
 
     def on_delete_enter(self, group: Group):
         ConfirmDialog(f'Удалить группу {group.group_name}?', self.confirmed_delete, group)
